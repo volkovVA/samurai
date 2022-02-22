4dd7a6ffab6dbcec94c75d4d65791756642efd90
volkovVA <volkov.slava@hotmail.com>
2022-02-22 18:57:14 +0300

    feat: add api request

--- react-social/src/components/Users/Users.js
+++ react-social/src/components/Users/Users.js
@@ -1,41 +1,30 @@
 import React from "react";
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/img/avatar2.png';
 
 const Users = ({ users, follow, unfollow, setUsers }) => {
 
-  if (users.length === 0) {
-    setUsers(
-      [
-        { id: 1,
-          photoUrl: 'https://media.baamboozle.com/uploads/images/58350/1593220552_125612',
-          followed: false,
-          fullName: 'Dmitry', status: 'I am Developer',
-          location: {city: 'Moscow', country: 'Russia'} },
-        { id: 2,
-          photoUrl: 'https://media.baamboozle.com/uploads/images/58350/1593220552_125612',
-          followed: true,
-          fullName: 'Elena',
-          status: 'I am Recruter',
-          location: {city: 'St.Petersburg', country: 'Russia'} },
-        { id: 3,
-          photoUrl: 'https://media.baamboozle.com/uploads/images/58350/1593220552_125612',
-          followed: false,
-          fullName: 'Alex',
-          status: 'I am CEO',
-          location: {city: 'Minsk', country: 'Belarus'} }
-      ]
-    )
+  const getUsers = () =>  {
+    if (users.length === 0) {
+      axios
+        .get('https://social-network.samuraijs.com/api/1.0/users')
+        .then(response => {
+          setUsers(response.data.items)
+        })
+    }
   }
 
   return (
     <div>
+      <button onClick={getUsers}>Get Users</button>
       {
         users.map(user => {
           return (
             <div key={user.id}>
               <span>
                 <div>
-                  <img src={user.photoUrl} className={styles.userPhoto} alt="avatar" />
+                  <img src={user.photos.small !== null ? user.photos.small : userPhoto} className={styles.userPhoto} alt="avatar" />
                 </div>
                 <div>
                   {user.followed
@@ -46,12 +35,12 @@ const Users = ({ users, follow, unfollow, setUsers }) => {
               </span>
               <span>
                 <span>
-                  <div>{user.fullName}</div>
+                  <div>{user.name}</div>
                   <div>{user.status}</div> 
                 </span>
                 <span>
-                  <div>{user.location.country}</div>
-                  <div>{user.location.city}</div>
+                  <div>{'user.location.country'}</div>
+                  <div>{'user.location.city'}</div>
                 </span>
               </span>
             </div> 
