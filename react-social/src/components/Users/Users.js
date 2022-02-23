7b510629351189e7729ae4c2c343e6ebae18f84a
volkovVA <volkov.slava@hotmail.com>
2022-02-23 21:24:42 +0300

    refactor: change users component

--- react-social/src/components/Users/Users.js
+++ react-social/src/components/Users/Users.js
@@ -1,86 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from './Users.module.css';
-import * as axios from 'axios';
 import userPhoto from '../../assets/img/avatar2.png';
 
-class Users extends Component {
+const Users = (props) => {
+  const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  const pages = [];
 
-  componentDidMount() {
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-      .then(response => {
-        this.props.setUsers(response.data.items);
-        console.log(response.data);
-        this.props.setTotalUsersCount(response.data.totalCount - 17550);
-    })
+  for (let i = 0; i < pagesCount; i++) {
+    pages.push(i+1);
   }
 
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber);
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-      .then(response => {
-        this.props.setUsers(response.data.items)
-    })
-  }
-
-  render() {
-
-    const pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-    const pages = [];
-
-    for (let i = 0; i < pagesCount; i++) {
-      pages.push(i+1);
-    }
-
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          {
-            pages.map((page, idx) => {
-              return (
-                <span
-                  className={this.props.currentPage === page && styles.selectedPage} key={idx}
-                  onClick={ (e) => this.onPageChanged(page) }
-                >
-                  { page }
-                </span>
-              )
-            })
-          }
-        </div>
         {
-          this.props.users.map(user => {
+          pages.map((page, idx) => {
             return (
-              <div key={user.id}>
-                <span>
-                  <div>
-                    <img src={user.photos.small !== null ? user.photos.small : userPhoto} className={styles.userPhoto} alt="avatar" />
-                  </div>
-                  <div>
-                    {user.followed
-                      ? <button onClick={ () => this.props.unfollow(user.id) }>Unfollow</button>
-                      : <button onClick={ () => this.props.follow(user.id) }>Follow</button>
-                    }
-                  </div>
-                </span>
-                <span>
-                  <span>
-                    <div>{user.name}</div>
-                    <div>{user.status}</div> 
-                  </span>
-                  <span>
-                    <div>{'user.location.country'}</div>
-                    <div>{'user.location.city'}</div>
-                  </span>
-                </span>
-              </div> 
+              <span
+                className={props.currentPage === page ? styles.selectedPage : ''} key={idx}
+                onClick={ () => props.onPageChanged(page) }
+              >
+                { page }
+              </span>
             )
           })
         }
       </div>
-    )
-  }
+      {
+        props.users.map(user => {
+          return (
+            <div key={user.id}>
+              <span>
+                <div>
+                  <img src={user.photos.small !== null ? user.photos.small : userPhoto} className={styles.userPhoto} alt="avatar" />
+                </div>
+                <div>
+                  {user.followed
+                    ? <button onClick={ () => props.unfollow(user.id) }>Unfollow</button>
+                    : <button onClick={ () => props.follow(user.id) }>Follow</button>
+                  }
+                </div>
+              </span>
+              <span>
+                <span>
+                  <div>{user.name}</div>
+                  <div>{user.status}</div> 
+                </span>
+                <span>
+                  <div>{'user.location.country'}</div>
+                  <div>{'user.location.city'}</div>
+                </span>
+              </span>
+            </div> 
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default Users;
--- react-social/src/components/Users/UsersContainer.js
+++ react-social/src/components/Users/UsersContainer.js
@@ -1,3 +1,5 @@
+import React, { Component } from "react";
+import * as axios from 'axios';
 import Users from "./Users";
 import { connect } from "react-redux";
 import {
@@ -7,6 +9,38 @@ import {
   setCurrentPageActionCreater,
   setTotalUsersCountActionCreater
 } from "../../redux/users-reducer";
+
+class UsersContainer extends Component {
+  
+  componentDidMount() {
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount - 17550);
+    })
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.items)
+    })
+  }
+
+  render() {
+    return <Users
+              totalUsersCount={this.props.totalUsersCount}
+              pageSize={this.props.pageSize}
+              currentPage={this.props.currentPage}
+              onPageChanged={this.onPageChanged}
+              users={this.props.users}
+              follow={this.props.follow}
+              unfollow={this.props.unfollow} />
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -37,4 +71,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
