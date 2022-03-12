347335a6e735c47bfddcff423ff5d94d9a65d985
volkovVA <volkov.slava@hotmail.com>
2022-03-12 21:41:45 +0300

    feat: add initialize app

--- react-social/src/App.js
+++ react-social/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import { Container, Row, Col } from "react-bootstrap";
+import { connect } from "react-redux";
 import "./App.css";
 
 import HeaderContainer from "./components/Header/HeaderContainer";
@@ -12,36 +13,59 @@ import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import UsersContainer from "./components/Users/UsersContainer";
 import Login from "./components/Login/Login";
+import { initializeApp } from "./redux/app-reducer";
+import FadeLoader from "react-spinners/FadeLoader";
 
-const App = (props) => {
-  return (
-    <BrowserRouter>
-      <Container>
-        <Row className="bg-primary">
-          <Col>
-            <HeaderContainer />
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={4} className="bg-info">
-            <Navbar />
-          </Col>
-          <Col sm={8} className="bg-warning">
-            <Route
-              path="/profile/:userId?"
-              render={() => <ProfileContainer />}
-            />
-            <Route path="/dialogs" render={() => <DialogsContainer />} />
-            <Route path="/users" render={() => <UsersContainer />} />
-            <Route path="/login" render={() => <Login />} />
-            <Route path="/news" component={News} />
-            <Route path="/music" component={Music} />
-            <Route path="/settings" component={Settings} />
-          </Col>
-        </Row>
-      </Container>
-    </BrowserRouter>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.initializeApp();
+  }
 
-export default App;
+  render() {
+    if (!this.props.initialized) {
+      return (
+        <FadeLoader
+          color="#0d6efd"
+          height={25}
+          width={5}
+          radius={15}
+          margin={10}
+          speedMultiplier={1.5}
+        />
+      );
+    }
+
+    return (
+      <BrowserRouter>
+        <Container>
+          <Row className="bg-primary">
+            <Col>
+              <HeaderContainer />
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={4} className="bg-info">
+              <Navbar />
+            </Col>
+            <Col sm={8} className="bg-warning">
+              <Route
+                path="/profile/:userId?"
+                render={() => <ProfileContainer />}
+              />
+              <Route path="/dialogs" render={() => <DialogsContainer />} />
+              <Route path="/users" render={() => <UsersContainer />} />
+              <Route path="/login" render={() => <Login />} />
+              <Route path="/news" component={News} />
+              <Route path="/music" component={Music} />
+              <Route path="/settings" component={Settings} />
+            </Col>
+          </Row>
+        </Container>
+      </BrowserRouter>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({ initialized: state.app.initialized });
+
+export default connect(mapStateToProps, { initializeApp })(App);
--- react-social/src/redux/redux-store.js
+++ react-social/src/redux/redux-store.js
@@ -5,6 +5,7 @@ import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
 import { reducer as formReducer } from "redux-form";
+import appReducer from "./app-reducer";
 
 const reducers = combineReducers({
   profilePage: profileReducer,
@@ -12,6 +13,7 @@ const reducers = combineReducers({
   usersPage: usersReducer,
   auth: authReducer,
   form: formReducer,
+  app: appReducer,
 });
 
 const store = createStore(reducers, applyMiddleware(thunkMiddleware));
