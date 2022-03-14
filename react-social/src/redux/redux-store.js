d6b16b3491d9f51e87a6b1cdac38026a53203bc6
volkovVA <volkov.slava@hotmail.com>
2022-03-14 18:44:48 +0300

    feat: add redux extension

--- react-social/src/redux/redux-store.js
+++ react-social/src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore, compose } from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
@@ -16,7 +16,13 @@ const reducers = combineReducers({
   app: appReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
+
+// const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 
 window.store = store;
 
